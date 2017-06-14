4313d0160a6e8a096e3c4a5cf6d19738152fea61
Jagger.Yang <rjagge@163.com>
2017-06-14 23:57:41 +0800

    init chicken

---
+++ view/index/page2.php
@@ -0,0 +1,9 @@
+<div class="app-page" data-page="page2">
+      <div class="app-topbar">
+        <div class="app-button left" data-back data-autotitle></div>
+        <div class="app-title">Page 2</div>
+      </div>
+      <div class="app-content">
+        Page 2 is lonely
+      </div>
+    </div>
