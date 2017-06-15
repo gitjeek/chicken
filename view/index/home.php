3338fcd00b09093018a780f6b8219c7bc1f73853
Jagger.yang <rjagge@163.com>
2017-06-15 20:32:06 +0800

    all

---
+++ view/index/home.php
@@ -0,0 +1,30 @@
+﻿<div class="app-page" data-page="home">
+
+	<div class="app-topbar">
+		<div class="app-title">这是乐博主页</div> 
+	</div>
+
+	<div class="app-content">
+		<!-- <div class="app-section">
+			<div class="app-button" data-target="page2">Go to Page 2</div>
+		</div> -->
+
+		<div class="col-lg-10">
+			<div class="input-group">
+				<input type="text" class="form-control" placeholder="Search for...">
+				<span class="input-group-btn">
+					<button class="btn btn-default" type="button">Go!</button>
+				</span>
+			</div><!-- /input-group -->
+		</div>
+
+
+
+
+	</div>
+
+	
+
+
+	
+</div>
---
+++ view/public/header.php
@@ -0,0 +1,12 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui">
+	<link rel="stylesheet" href="css/public/app.min.css">
+	<link rel="stylesheet" href="public/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="public/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css">
+	<style>
+		/* TODO */
+	</style>
+</head>
+<body>  
